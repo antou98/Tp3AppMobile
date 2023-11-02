09c5187b0980865588df3db7d3b95e2d0a605979
antou98 <114108339+antou98@users.noreply.github.com>
2023-11-02 18:23:10 -0400

    ajout

--- node/server.js
+++ node/server.js
@@ -15,18 +15,35 @@ var connPool = mysql.createPool({
     port : "20509"
 });
 
-
-/*app.post("/connect/:mail/:password",(req,res)=>{
-
-})*/
-
-
-
-
 app.get("/api",(req,res)=>{
     console.log("connection");
     res.send({"val":"succes"}).end();
 })
+
+app.get("/api/connect/:mail/:password", (req, res) => {
+    connPool.getConnection((err, connection) => {
+        if (err) {
+            res.status(500).json({ error: "Database connection error" });
+            return;
+        }
+
+        let query = "select * from FROM defaultdb.user u WHERE u.email = ? AND u.password = ?";
+        connPool.query(query,[req.params.mail, req.params.password],(err, rows, fields)=>{
+            if (err){
+                res.status(500).json({ error: "Database query error" });
+            }
+
+            if(rows!==undefined){
+                res.json({ message: "Connected to the database" });
+            }
+            else{
+                res.json({ message: "no data " });
+            }
+            
+        });
+        connection.release();
+    });
+});
 
 app.listen(5000,()=>{
     console.log("listening on port 5000");
